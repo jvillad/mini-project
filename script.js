5c71f3253dd13425787b06ca8fdf691e7b93aeb1
jvillad <jyvillad@gmail.com>
2022-12-07 14:11:12 +1100

    update

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ const addBtn = document.querySelector('.add-btn')
 const content = document.querySelector('.content');
 
 addBtn.addEventListener('click', function getValue (ev) {
-    
+    ev.preventDefault();
     const userTask = document.querySelector('input').value;
     if(userTask === '') {
         return; 
     } else {
         //TODO: use/check localStorage
-        displayTask(userTask);
+        renderTask(userTask);
+        document.querySelector('input').value = '';
     }
 
 })
 
-function displayTask(task) {
+function renderTask(task) {
 
     // get ul from content class
     const uList = document.querySelector('.content ul');
@@ -21,23 +22,23 @@ function displayTask(task) {
     // li element for ul
     const li = document.createElement('li');
     
-    // button element for li
+    // done button element for li
     const doneBtn = document.createElement('button');
     doneBtn.classList.add('done');
     doneBtn.innerText = 'Done';
     
+    // delete task button element for li
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('delete');
     deleteBtn.innerText = 'Delete';
 
-    li.innerText = task;
     li.appendChild(doneBtn);
+    li.append(task);
     li.appendChild(deleteBtn);
-    uList.appendChild(li);
+    uList.append(li);
     
-    // TODO: Add validation for done task 
-    // doneTask(doneBtn);
-    // deleteTask(deleteBtn);
+    doneTask(doneBtn);
+    deleteTask(deleteBtn);
 
 }
 
@@ -48,16 +49,20 @@ function deleteTask(deleteBtn) {
         if (!ev.target.matches('.delete')) {
             return;
         }
-        
         ev.target.closest('li').remove();
-  
     })
 }
 
 function doneTask(doneBtn) {
+
     doneBtn.addEventListener('click', function (ev) {
-        const doneTasks = document.querySelector('.done-content')
-        doneTasks.innerText = ev.target.previousSibling.wholeText;
+        
+        const doneTasks = document.querySelector('.done-content ul')
+        const li = document.createElement('li');
+        
+        li.innerHTML = ev.target.nextSibling.wholeText.strike();
+        doneTasks.append(li);
+        ev.target.closest('li').remove();
         
     })
 
